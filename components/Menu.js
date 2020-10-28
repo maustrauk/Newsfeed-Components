7746cd58a2723dc0fbf175c14a97ac4f1e14dfe9
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-10-28 19:01:04 -0400

    Part_3 Step_4-5

--- components/Menu.js
+++ components/Menu.js
@@ -24,6 +24,10 @@ let menuItems = [
   Add those items to the <ul>
 
   Step 3: Still inside your function, select from the DOM the menu button (the element with a class of 'menu-button').
+
+  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
+
+  Step 5: Don't forget to return your div.menu.
 */
 
 function menuMaker (menuArray) {
@@ -46,20 +50,14 @@ function menuMaker (menuArray) {
   });
   
   const menuButton = document.querySelector('menu-button');
-  
+
+  menuButton.addEventListener('click', () => menu.classList.toggle('menu--open'));
+
 
   return menu;
 }
 
 
 /*
-  
-
-  
-
-  Step 4: Add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on div.menu (your div with a 'menu' class).
-
-  Step 5: Don't forget to return your div.menu.
-
   Step 6: Use 'menuMaker' to create a menu using the 'menuItems' array, and append the returned menu to the header.
 */
